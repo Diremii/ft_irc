5a93f22711ac5c6f4e55476f2ee8fa7f84e57832
Hugo <144733665+Diremii@users.noreply.github.com>
2026-05-23 21:19:52 +0200

    Refactor User class method names and parameters

--- includes/User.hpp
+++ includes/User.hpp
@@ -32,17 +32,17 @@ class User
 
 		User	&operator=(const User &other);
 
-		int			get_user_fd(void) const;
-		std::string	get_nick_name(void) const;
-		std::string	get_user_name(void) const;
-		std::string	get_host_name(void) const;
-		bool		get_authenticated(void) const;
+		int			getFd(void) const;
+		std::string	getNickname(void) const;
+		std::string	getUsername(void) const;
+		std::string	getHostname(void) const;
+		bool		getAuthenticated(void) const;
 		
-		void	set_user_fd(int user_fd);
-		void	set_nick_name(std::string &nick_name);
-		void	set_user_name(std::string &user_name);
-		void	set_host_name(std::string &host_name);
-		void	set_authenticated(bool auth);
+		void	setFd(int userFd);
+		void	setNickname(std::string &nickname);
+		void	setUsername(std::string &username);
+		void	setHostname(std::string &hostname);
+		void	setAuthenticated(bool auth);
 		
 };
 
